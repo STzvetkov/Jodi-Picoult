a7f6192ab8a56eb65b6ee1ae5bb3478ea173d704
Simeon Tzvetkov <simeon_tzvetkov@mail.bg>
2014-02-26 00:39:28 +0200

    LandObject : implements IDestroyable

--- PirateGame/PirateGame/MapObjects/LandObject.cs
+++ PirateGame/PirateGame/MapObjects/LandObject.cs
@@ -5,15 +5,28 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using PirateGame.Interfaces;
 
 namespace PirateGame.MapObjects
 {
-    public abstract class LandObject:MapObject
+    public abstract class LandObject:MapObject, IDestroyable
     {
 
+        // constructors
         public LandObject(ContentManager content, string texture, int x, int y, int width, int height) : base(content, texture, x, y, width, height)
         {
             
+        }
+
+        // properties
+        public int Hitpoints { get; private set; }
+        public bool IsDestroyed { get; private set; }
+
+        // methods
+        public void TakeDamage(int damageCaused)
+        {
+            this.Hitpoints -= damageCaused;
+            this.IsDestroyed = this.Hitpoints <= 0;
         }
     }
 }
